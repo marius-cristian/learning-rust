5b90d47b308f6de48611d785c1e4f93edaf9a246
mfc <cristian.marius.florin@protonmail.com>
2020-06-25 18:41:20 +0200

    chapter rushed

--- book_of_rust/io_project_minigrep/src/main.rs
+++ book_of_rust/io_project_minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use io_project_minigrep::{run, Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,37 +20,12 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename); // --snip--
-}
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.filename);
 
-fn run(config: Config) {
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
 
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+        process::exit(1);
     }
 }
