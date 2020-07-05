8b02ba669a86ca6ab307a90d970d33e1329b1231
mfc <cristian.marius.florin@protonmail.com>
2020-07-05 13:47:16 +0200

    lifetimes chapter finished

--- book_of_rust/generic_traits_lifetime/src/references_with_lifetimes/lib.rs
+++ book_of_rust/generic_traits_lifetime/src/references_with_lifetimes/lib.rs
@@ -4,9 +4,46 @@ pub fn test_main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    let s: &'static str = "I have a static lifetime.";
 }
 
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
     } else {
